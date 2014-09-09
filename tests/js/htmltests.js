2803f5f3947db5f924048fcf417048042204b5b1
frankiefu <frankiefu@yahoo.com>
2014-09-09 15:33:27 -0700

    move tests from core-tests

---
+++ tests/js/htmltests.js
@@ -0,0 +1,5 @@
+htmlSuite('core-selector', function() {
+  htmlTest('html/core-selector-basic.html');
+  htmlTest('html/core-selector-activate-event.html');
+  htmlTest('html/core-selector-multi.html');
+});
