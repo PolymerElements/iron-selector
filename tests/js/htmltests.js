a0f407ae6cda63e8ced5e5442177b0fa26ff6198
frankiefu <frankiefu@yahoo.com>
2014-09-10 11:18:43 -0700

    more tests

--- tests/js/htmltests.js
+++ tests/js/htmltests.js
@@ -2,4 +2,7 @@ htmlSuite('core-selector', function() {
   htmlTest('html/core-selector-basic.html');
   htmlTest('html/core-selector-activate-event.html');
   htmlTest('html/core-selector-multi.html');
+  htmlTest('html/core-selector-next-previous.html');
+  htmlTest('html/core-selector-next-previous-wrap.html');
+  htmlTest('html/core-selector-selected-attr-prop.html');
 });
