e945bd9b7d7c7e03f57fa647360c0a4ceccca330
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:40 -0700

    auto-converted by polymer-modulizer

--- test/content-element.js
+++ test/content-element.js
@@ -1,4 +1,4 @@
-<!--
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,38 +6,29 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
+*/
+import '../iron-selector.js';
 
-<link rel="import" href="../iron-selector.html">
-
-<dom-module id="test-content-element">
-
-  <template>
-
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
     <iron-selector id="selector" selected="{{selected}}" selectable="[[selectable]]" attr-for-selected="id">
       <slot></slot>
     </iron-selector>
+`,
 
-  </template>
+  is: 'test-content-element',
 
-</dom-module>
+  properties: {
 
-<script>
-  Polymer({
+    selectable: String,
 
-    is: 'test-content-element',
+    selected: {type: String, notify: true}
 
-    properties: {
+  },
 
-      selectable: String,
-
-      selected: {type: String, notify: true}
-
-    },
-
-    get selector() {
-      return this.$.selector;
-    },
-
-  });
-</script>
+  get selector() {
+    return this.$.selector;
+  }
+});
