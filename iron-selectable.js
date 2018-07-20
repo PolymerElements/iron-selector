50e195654f3a5a62d5545981d4fb9806da978c27
Russell Bicknell <bicknellr@google.com>
2018-07-20 12:20:34 -0700

    format

--- iron-selectable.js
+++ iron-selectable.js
@@ -1,17 +1,19 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
 import '@polymer/polymer/polymer-legacy.js';
 
-import { IronSelection } from './iron-selection.js';
-import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
-import { dashToCamelCase } from '@polymer/polymer/lib/utils/case-map.js';
+import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
+import {dashToCamelCase} from '@polymer/polymer/lib/utils/case-map.js';
+
+import {IronSelection} from './iron-selection.js';
 
 /**
  * @polymerBehavior Polymer.IronSelectableBehavior
@@ -143,8 +145,7 @@ export const IronSelectableBehavior = {
 
   created: function() {
     this._bindFilterItem = this._filterItem.bind(this);
-    this._selection =
-        new IronSelection(this._applySelection.bind(this));
+    this._selection = new IronSelection(this._applySelection.bind(this));
   },
 
   attached: function() {
@@ -266,8 +267,7 @@ export const IronSelectableBehavior = {
   },
 
   _updateItems: function() {
-    var nodes =
-        dom(this).queryDistributedElements(this.selectable || '*');
+    var nodes = dom(this).queryDistributedElements(this.selectable || '*');
     nodes = Array.prototype.filter.call(nodes, this._bindFilterItem);
     this._setItems(nodes);
   },
--- iron-selector.js
+++ iron-selector.js
@@ -1,16 +1,19 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
 import '@polymer/polymer/polymer-legacy.js';
 
-import { IronMultiSelectableBehavior } from './iron-multi-selectable.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
+
+import {IronMultiSelectableBehavior} from './iron-multi-selectable.js';
+
 /**
 `iron-selector` is an element which can be used to manage a list of elements
 that can be selected.  Tapping on the item will make the item selected.  The
